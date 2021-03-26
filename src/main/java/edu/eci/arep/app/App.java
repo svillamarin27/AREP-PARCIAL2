962e50ee7bb18e02266c8d150a1d02edf4075e44
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-03-26 14:05:05 -0500

    aplicacion

--- src/main/java/edu/eci/arep/app/App.java
+++ src/main/java/edu/eci/arep/app/App.java
@@ -6,18 +6,28 @@ import static spark.Spark.*;
 
 
 public class App {
-
+    
 	private static Calculator calculate = new Calculator(); 
-
-
+	
+	
 	public static void main( String[] args ){
-
 		port(getPort());
-        get("/parcial", (req, res) ->("parcial"));
-
+		get("/sqrt", (req, res) -> inputDataPage(req, res,"sqrt"));//8
+        get("/sin", (req, res) -> inputDataPage(req, res,"sin"));//2
+        
     }
 
-
+	//http://localhost:4567/sin?valor=4
+	private static JSONObject inputDataPage(Request req, Response res,String operation) {
+        double num = Double.parseDouble(req.queryParams("valor"));
+        JSONObject obj = new JSONObject();
+        obj.put("output",(operation.equals("sqrt"))? calculate.sqrtFunction(num) : calculate.sinFunction(num));        
+        obj.put("input",num);
+        obj.put("operation",operation); 
+        return obj;
+    }
+	
+	
 	static int getPort() {
 	       if (System.getenv("PORT") != null) {
 	           return Integer.parseInt(System.getenv("PORT"));
--- src/main/java/edu/eci/arep/app/Calculator.java
+++ src/main/java/edu/eci/arep/app/Calculator.java
@@ -1,5 +1,15 @@
 package edu.eci.arep.app;
 
+
 public class Calculator {
 
+	
+	public double sqrtFunction(double number){
+        return Math.sqrt(number);
+    }
+	
+	public double sinFunction(double number){
+        return Math.sin(number);
+    }
+	
 }
